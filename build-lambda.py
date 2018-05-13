0649c91d20cbfb6aa8ce021b380de2d5b458ad3e
Sean6bucks <sean6bucks@gmail.com>
2018-05-13 10:21:18 +0200

    update build script

--- build-lambda.py
+++ build-lambda.py
@@ -12,8 +12,9 @@ portfolio_zip = StringIO.StringIO()
 build_bucket.download_fileobj('portfolioBuild.zip', portfolio_zip)
 
 with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
+    build = myzip.open('build')
+    for nm in build.namelist():
+        obj = build.open(nm)
         portfolio_bucket.upload_fileobj(obj, nm)
         ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]}
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
